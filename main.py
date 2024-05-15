3f34548a99568024966560fa529edf2305d1d205
adam <twoj@example.com>
2024-05-15 13:38:12 +0200

    Complete creating category page and start creating proudct page

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ with app.app_context():
 def index_page():
     products = Product.query.all()
     products1 = Product.query.filter_by(category='garden')[:10]
-    products2 = Product.query.filter_by(category='art')[:10]
+    products2 = Product.query.filter_by(category='kids')[:10]
     products3 = Product.query.order_by(Product.price.asc()).all()
     carousel_items = [products[i:i+3] for i in range(0, len(products), 3)]
 
@@ -35,6 +35,24 @@ def index_page():
 
     return render_template('index.html', products1=products1, products2=products2, products3=products3, carousel_items=carousel_items, products4=products3 )
 
+@app.route('/category/<category>')
+def category(category):
+    
+    products = Product.query.all()
+    #category_lower = str(category_path.lower())
+    #print(category_lower)
+    category_products = Product.query.filter_by(category=str(category.lower()))
 
+    products1 = Product.query.filter_by(category='kids')[:10]
+    return render_template('category.html', products=category_products)
+
+
+@app.route("/product/<int:id>", methods=['GET'])
+def product(id):
+    print(id)
+    product = Product.query.filter_by(id=id).first()
+    print(product)
+    return render_template("product.html", product=product)
 if __name__ == "__main__":
     app.run()
+
